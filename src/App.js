b64df2fc3bf804883b037ac86b9b5a2c2e423ae5
shawsourav <shawsourav91@gmail.com>
2019-06-22 13:15:39 +0530

    [WIP] more changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
+import Header from './header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <Header/>
     </div>
   );
 }
---
+++ src/header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import WebDev from './assets/web_dev.png';
+
+const Header = (props) => {
+    return (
+        <header>
+            <img src={WebDev}/>
+        </header>
+    )
+}
+
+export default Header;
